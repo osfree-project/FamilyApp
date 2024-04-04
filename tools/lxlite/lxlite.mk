be7f15e252ff1f901e679d27741a536066abceae
prokushev <yuri.prokushev@gmail.com>
2024-04-04 13:08:06 +0700

    stub for dualmode attrib

--- tools/lxlite/lxlite.mk
+++
@@ -1,9 +0,0 @@
-# $Id: makefile,v 1.1 2004/08/16 06:27:05 prokushev Exp $
-
-PORT_NAME = tools$(SEP)lxlite
-PORT_TYPE = git
-PORT_URL  = https://github.com/bitwiseworks/lxlite
-PORT_REV  = master
-PORT_PATCHES  = lxlite.diff
-
-!include $(%ROOT)tools/mk/port.mk
--- tools/shared/z/zlib.mk
+++
@@ -1,9 +0,0 @@
-# $Id: makefile,v 1.1 2004/08/16 06:27:05 prokushev Exp $
-
-PORT_NAME = tools$(SEP)zlib
-PORT_TYPE = wget
-PORT_URL  = https://www.zlib.net/zlib131.zip
-PORT_REV  = 
-PORT_PATCHES  = 
-
-!include $(%ROOT)tools/mk/port.mk
